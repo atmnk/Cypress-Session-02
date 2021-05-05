73c30d24232fafcf24222cc39a6de2852f0797b0
ATM <naik.atmaram@gmail.com>
2021-05-05 18:31:21 +0530

    ATM | created starting point

--- cypress/integration/04-AdvanceConcepts/13.js
+++ cypress/integration/04-AdvanceConcepts/13.js
@@ -1,27 +1,10 @@
 import * as HomePage from '../../pages/Home'
 describe('Stub Some APIS',()=>{
     it('Stub Global Feeds 0',()=>{
-        cy.fixture('articles.zero').then(body=>{
-            cy.intercept('https://tw-cypress-be.herokuapp.com/api/articles?limit=10&offset=0',{
-                statusCode: 200,
-                body,
-              }).as('ArticleAPI')
-            HomePage.visit();
-            cy.wait('@ArticleAPI')
-            HomePage.verifyGlobalFeed(body.articles)
-        })
+        
     })
     it('Stub Global Feeds many',()=>{
-        cy.fixture('articles.many').then(pattern=>{
-            let body = getArticles(pattern,10)
-            cy.intercept('https://tw-cypress-be.herokuapp.com/api/articles?limit=10&offset=0',{
-                statusCode: 200,
-                body,
-              }).as('ArticleAPI')
-            HomePage.visit();
-            cy.wait('@ArticleAPI')
-            HomePage.verifyGlobalFeed(body.articles)
-        })
+        
     })
 })
 function getArticles(pattern,count){
